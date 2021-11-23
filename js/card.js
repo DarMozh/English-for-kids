540e82e25f90a0f387c855f9ca6f769e4f05ee28
KatmannikCore <73733601+KatmannikCore@users.noreply.github.com>
2021-11-23 16:11:22 +0300

    First

---
+++ js/card.js
@@ -0,0 +1,13 @@
+class Card {
+    word;
+    translation;
+    image;
+    audioSrc;
+    constructor(word,translation,image,audioSrc) { 
+        this.word = word;
+        this.translation = translation;
+        this.image = image;
+        this.audioSrc = audioSrc;
+    }
+ 
+  }
